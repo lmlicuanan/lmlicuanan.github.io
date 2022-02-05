03998a6adc0eaf5913aae763f15f090c12d1e89b
Lino Licuanan <lmlicuanan14@gmail.com>
2022-02-05 09:25:13 +0800

    final draft

--- _posts/2022-02-03-demystifying-openstreetmap-data/_targets_r/targets/random-data.R
+++ _posts/2022-02-03-demystifying-openstreetmap-data/_targets_r/targets/random-data.R
@@ -1,13 +1,13 @@
 list(
   tar_target(
-    mc_point.sf, st_as_sf(st_sample(city.sf$osm_multipolygons, 22 * 16))
+    mc_point.sf, st_as_sf(st_sample(city.sf$osm_multipolygons, 1000))
   ),
   tar_target(
     mc_point_params.sf, aggregate(
       amenity_point.sf,
       mc_point.sf,
       FUN = function(x) sum(as.logical(x), na.rm = TRUE),
-      join = function(x, y) st_is_within_distance(x, y, dist = 250)
+      join = function(x, y) st_is_within_distance(x, y, dist = 500)
     ) %>% filter(!is.na(amenity))
   )
 )
